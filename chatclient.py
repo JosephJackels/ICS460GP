036c38af226edced2dfdafac0242526efe9034a3
Joseph Jackels <jackelsjoseph@gmail.com>
2021-11-20 19:08:11 -0600

    Added comment blocks for each file

--- chatclient.py
+++ chatclient.py
@@ -1,26 +1,57 @@
 from socket import *
 import sys, threading, queue
 
-#recieve thread
+
+#Joseph Jackels, Punyapat Rabalert
+#StarID: dn0012nk, ih2153wu
+#
+#
+#
+#This file is the client that can connect to the server in order to send/receive chat messages with other users
+#It has 3 paramaters <hostname> <port> and <username> - the address of the server, the port to connect to it on, and the username to login as
+#
+#The main thread creates TWO sockets, a TCP socket and a UDP socket, both listen on the same port/host
+#The TCP socket is used for sending operation commands to the server, and receiving responses necessary for carrying out those commands/operations
+#The UDP socket is used only for receiving data messages from the server and saving them to be printed when appropriate
+#
+#The main thread checks via the server if the user is existing, new, or already active and handles each appropriately
+#	-existing
+#		prompt user for password, validate via the server
+#	-new
+#		prompt user for new password, validate that it is using safe characters, send to server for saving
+#	-active
+#		user is already logged in somewhere else, notify and end program
+#
+#After handling logging in, the main thread begins a thread for listening for data messages over the UDP socket
+#
+#The main thread then prompts the user for operations and executes them
+#	-PM
+#		prompts user for a public message to be sent to all other active users
+#	-DM
+#		gets a list of active users from the server and prompts the user for a user to send a message to and the message to be sent
+#	-EX
+#		logs the user out of the server
+#
+#Once the operation thread begins and data messages can be received, 
+#	Whenever the main thread is waiting for input or for a response from the server, it starts threads to wait for the input/response instead
+#	This allows for the queue of messages to be continuously checked and printed while waiting for the response/input to be received. The child thread then flags that it is ready
+#	and the input/response is returned to the main thread
+
+#data message receving thread
+#receives data messages via the udpSocket
+#adds them to the queue of messages to be printed out at the proper time
 def message_recieving_thread(udpSocket):
 	global messageQueue
-	#listen for PM/DM being sent to this socket
-	#decide when to print?
 	
-	# maybe we should make a global queue that contains (message, address)
-	# this thread can simply recieve messages and add to the queue
-	# then the main thread above can decide when it is a "safe" time
-	# to print any pending messages, e.x. after a command is processed it will print all messages?
-	# we could even have a third 'printing' thread that handles printing for both the main operation thread and this thread 
-
+	#listen for messages forever and save them to the messageQueue
+	#this thread should only be run as a daemon so it will end when its parent does
 	while True:
 		message, address = udpSocket.recvfrom(4096)
 		userFrom, address = udpSocket.recvfrom(4096)
 		messageType, address = udpSocket.recvfrom(4096)
 		messageQueue.put((message.decode(), userFrom.decode(), messageType.decode()))
-		#do something with message and username of sender.
-		#either print it out now, or add to queue to print when able
 
+#this function is to be used by a thread that waits for input and prompts it's parent that the input is ready
 def input_listener_thread(prompt):
 	global inputCommand
 	global inputReadyFlag
@@ -28,6 +59,9 @@ def input_listener_thread(prompt):
 	inputCommand = input(prompt)
 	inputReadyFlag = True
 
+#starts an input_listener_thread and waits for input to be received, continually check messageQueue for data messages to print
+#if data messages are printed the prompt that is given for input is reprinted
+#returns the string that was input
 def get_input(prompt):
 	global inputReadyFlag
 	global inputCommand
@@ -42,6 +76,8 @@ def get_input(prompt):
 	inputReadyFlag = False
 	return inputCommand
 
+#prints all the messages in the message queue
+#reprints a given prompt. If no prompt is needed simply supply the function with an empty string
 def print_messages(prompt):
 	global messageQueue
 	while not messageQueue.empty():
@@ -52,6 +88,9 @@ def print_messages(prompt):
 		print(messageTuple[0] + "\n")
 		print(prompt)
 
+#starts a thread to listen for a socket response, to be used for getting operation responses over the tcp socket
+#prints out messages from the queue while waiting for the response
+#returns the string sent by the server
 def get_response(socket, prompt):
 	global responseReadyFlag
 	global socketResponse
@@ -66,37 +105,46 @@ def get_response(socket, prompt):
 	responseReadyFlag = False
 	return socketResponse
 
+#receives a response from the server and saves it, flags the parent thread that the response is ready
 def response_listener_thread(socket):
 	global responseReadyFlag
 	global socketResponse
 	socketResponse = socket.recv(4096).decode()
 	responseReadyFlag = True
 
+#check amount of given arguements
 if len(sys.argv) < 4:
 	print("Start the client via $python3 chatclient.py hostname port username")
 	sys.exit()
 
+#save given arguements
 hostname = sys.argv[1]
-port = int(sys.argv[2])
+try:
+	port = int(sys.argv[2])
+except ValueError:
+	print("Port must be given as an integer")
+	sys.exit()
 username = sys.argv[3]
 
 #queue for storing/printing DM/PM messages received
 messageQueue = queue.Queue()
 
+#attempt to begin and bind tcpsocket and begin udp socket
 try:
 	serverSocket = socket(AF_INET, SOCK_STREAM)
 	serverSocket.connect((hostname, port))
+	udpSocket = socket(AF_INET, SOCK_DGRAM)
 except ConnectionRefusedError:
 	print("TCP Connection was refused at host:", hostname, "on port:", port)
 	print("Did you use the order: hostname port requestedFile for the passed arguements?\nAre you using the correct host and port?")
+	print("Did you properly supply an address ex 127.0.1.1 and a port ex 1000? Are they properly formatted?")
 	sys.exit()
 
-# create a udp socket for recieving message etc.
-udpSocket = socket(AF_INET, SOCK_DGRAM)
-
+#send username to server
 serverSocket.send(username.encode())
 response = serverSocket.recv(4096).decode()
 
+#handle authentication of existing user
 if response == "existing":
 	approved = False
 	password = input("Existing user. Please enter password:\n")
@@ -112,6 +160,8 @@ if response == "existing":
 			sys.exit()
 		else:
 			approved = True
+
+#handle creation of new user
 elif response == "new":
 	password = input("Welcome new user: " + username + " please enter a new password:\n")
 
@@ -122,6 +172,7 @@ elif response == "new":
 	response = serverSocket.recv(4096).decode()
 	print(response)
 
+#username is already active
 elif response == "inuse":
 	print("That user is already actively logged in. Please restart the program as a different user")
 	serverSocket.close()
@@ -142,7 +193,6 @@ if(response != "udp recieve"):
 	print("Error receiving udp?")
 	print(response)
 
-stopEvent = threading.Event()
 #begin messageListening thread
 messageListenerThread = threading.Thread(target=message_recieving_thread, args=(udpSocket,), daemon=True)
 messageListenerThread.start()
@@ -153,12 +203,13 @@ socketResponse = ""
 inputReadyFlag = False
 inputCommand = ""
 
-#just recieved operation***
+#ready to send/receive operation messages with server
 operating = True
 while operating:
-
+	#get command
 	command = get_input("Please enter a command:\n\tPM - public message to all active users.\n\tDM - direct message to a  single user\n\tEX - exit program and logout of account\n")
 
+	#handle sending a public message
 	if command == "PM":
 		serverSocket.send(command.encode())
 		response = get_response(serverSocket, "")
@@ -174,6 +225,7 @@ while operating:
 		else:
 			print("Message sent")
 
+	#handle sending a direct message
 	elif command == "DM":
 		serverSocket.send(command.encode())
 		response = get_response(serverSocket, "")
@@ -187,38 +239,43 @@ while operating:
 
 			print("Select a user to send a message to:")
 			response = get_response(serverSocket, "")
-		
+			#receive list of active users
 			while response != "END":
 				print("\t" + response)
 				serverSocket.send("received".encode())
 				response = get_response(serverSocket, "")
-		
+			
+			#prompt user to select recipient of DM
 			sendTo = get_input("To:\n")
 			serverSocket.send(sendTo.encode())
 			response = get_response(serverSocket, "")
 		
+			#handle user Does Not Exist
 			if(response == "DNE"):
-				#handle Does Not Exist
 				print("User does not exist.")
 				serverSocket.send("received".encode())
 
+			#user exists, prompt user for message to send
 			elif(response == "message"):
 				gettingUser = False
 				#get message input, send it etc.
 				message = get_input("\nEnter message:\n")
 				serverSocket.send(message.encode())
 				response = get_response(serverSocket, "")
+				#message was not sent
 				if(response != "complete"):
 					print("Something went wrong")
+				#message sent
 				else:
 					print("Message sent")
 			else:
 				print("Invalid response?")
-		
+	#log user out	
 	elif command == "EX":
-		
+		#print and pending messages
 		print_messages("")
 
+		#log user out of server
 		operating = False
 		print("Sending logout command to server")
 		serverSocket.send(command.encode())
@@ -229,6 +286,7 @@ while operating:
 		else:
 			print("Error logging out??? closing socket anyways")
 
+		#close sockets and exit
 		serverSocket.close()
 		udpSocket.close()
 		sys.exit()
--- chatserver.py
+++ chatserver.py
@@ -3,19 +3,61 @@ import sys
 import threading
 import time
 import os.path
+
+#Joseph Jackels, Punyapat Rabalert
+#StarID: dn0012nk, ih2153wu
+#
+#
+#This file is the server, it receives connections from clients, handles logging them in/creating new users
+#And handling recieved operation requests and the sending of data messages.
+#
+#It has one parameter <port> - the port number to start the server on. must be given as an integer
+#
+#-main thread
+#	First, the input parameter is validated, sockets are bound, and the credentials are read and saved to a list.
+
+#	Then, main thread handles receiving new connections and keeps track of them in a list of threads that it periodically checks and updates to remove threads that are no longer running
+#	It will only allow new connections if the current amount of live connections is less then a given threshold (currently 10)
+#	When a new connection is accepted a thread is created, added to the list, and dispatched to handle client logging in, hndling operations, sending messages etc.
+#
+#-client thread
+#	The client thread handles checking if a user exists or not and if it is already active. 
+#	If a client exists and is not active the login process is handled by requesting a password and validating it.
+#	If a user is new a new password is requested and saved to the credentials file
+#	A successfully logged in/created user is added to the list of active users, along with some information about it's socket/address. then the operation loop begins
+#	The operation loop prompts the client for an operation to perform and handles the operation, along with prompting for any other necessary information and sending appropriate responses
+#
+#
+#
+#The server program has many lists for tracking things:
+#	connectionList - a list of currently active connection threads
+#	activeUsers - a list of currently active users
+#	credentialList - a list of username/password pairs that is retrieved from the file credentials.txt
+#
+#In order to allow threads to safely read and write to these same lists, the program contains a mutex lock for each list(except for connectionList because ONLY the main thread will access it)
+#	userListMutex - for accessing the activeUsers list
+#	credentialListMutex - for accessing the credentilList list
+#	fileMutex - for accessing the file credentials.txt
+
 #check that correct amount of paramaters are given
 if len(sys.argv) < 2:
 	print("please start server via $python3 chatserver.py port")
 	sys.exit()
 #set port variable
-port = int(sys.argv[1])
+try:
+	port = int(sys.argv[1])
+except ValueError:
+	print("Port must be given as an integer")
+	sys.exit()
 
 #create tcp socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
 udpSocket = socket(AF_INET, SOCK_DGRAM)
+
 #get hostname ex. 127.0.1.1
 host = gethostbyname(gethostname())
 
+#attempts to bind sockets to host/port, waiting until able
 connected = False
 print("Attempting to bind host/port to sockets", end="", flush=True)
 while not connected:
@@ -32,18 +74,19 @@ while not connected:
 print("\nserver listening at", host, "on port", port)
 serverSocket.listen(5)
 
+#max client connections at one time
 maxConnections = 10
 
 #list of dispatched threads for handling messages
 connectionList = list() #[thread1, thread2, thread3...]
 
-#list of currently active users, along with their assigned socket
+#list of currently active users, along with their assigned socket and address for sending udp messages
 activeUsers = list() #[(joe, joeTcpSocket, joeUdpAddress), (bill, billSocket, billUdpAddress), (beth, bethSocket, bethUdpAddress)...]
 
 tempList = list() # for clearing /copying lists
 
 #list of saved username/password pairs
-credentialList = list() #[(joe, password), (bill, password1), (beth, password2)] READ IN FROM FILE
+credentialList = list() #[(joe, password), (bill, passwordBill), (beth, passwordBeth)] READ IN FROM FILE
 
 #mutexes (mutual exclusion locks) for accessing relevent lists without threading issues
 userListMutex = threading.Lock()
@@ -59,7 +102,7 @@ try:
 		credentialList.append((line.split()[0], line.split()[1]))#add (user, pass) to credential list
 	credentialFile.close()
 except IOError:
-	#File not found, try to create file
+	#File not found, try to create file?
 	try:
 		credentialFile = open('credentials.txt', 'w')
 	except IOError:
@@ -162,8 +205,7 @@ def client_connection_thread(clientSocket):
 	#user is logged in / registered
 	
 	# recieve message from the udp socket
-	# save somewhere in the active userList the udp address for sending messages to
-	# USERlIST WILL HAVE TO BE CHANGED TO SOMETHING LIKE (username, tcpSocket, udpSocket)
+	# save in the active userList the udp address for sending messages to
 	messageType, address = udpSocket.recvfrom(4096)
 
 	#append them to active user list
@@ -176,42 +218,38 @@ def client_connection_thread(clientSocket):
 	clientSocket.send("udp recieve".encode())
 	
 	while running:
-		#prompt client for operation
-		#clientSocket.send("operation".encode())
-		
 		#wait for command
 		operation = clientSocket.recv(4096).decode()
 		
 		if operation == "PM":
 			#PM - broadcast to all active () logged in and not yet exitted
 			#send acknowledgement of PM request
-			#prompt for message
 			clientSocket.send("PM".encode())
 			#wait for message
 			message = clientSocket.recv(4096).decode()
 
-			#pmThread = threading.Thread(target=pm_message, args=(message, (username, clientSocket)))
-			#pmThread.start()
+			#check active user list for any users that aren't the sender, and send them the message
 			userListMutex.acquire()
 			for userTuple in activeUsers:
 				if userTuple[0] != username:
-					#userTuple[1].send(message.encode())
 					udpSocket.sendto(message.encode(), userTuple[2])
 					udpSocket.sendto(username.encode(), userTuple[2])
 					udpSocket.sendto("Public Message (PM)".encode(), userTuple[2])
 			userListMutex.release()
+
+			#notify user that message was sent
 			clientSocket.send("complete".encode())
 			#return to wait for command
 
 		elif operation == "DM":
-			#DM
 				#acknowledge DM request
 				clientSocket.send("DM".encode())
 				message = clientSocket.recv(4096).decode()
 				if(message != "received"):
 					print("Error handshaking?")
 					print(message)
-				
+
+				#send user a list of all active users
 				gettingUser = True
 				while gettingUser:
 					userListMutex.acquire()
@@ -223,10 +261,12 @@ def client_connection_thread(clientSocket):
 					userListMutex.release()
 
 					clientSocket.send("END".encode())
+
+					#get recipent from client
 					reciever = clientSocket.recv(4096).decode()
 
+					#search list of active users for recipent
 					found = False
-				
 					userListMutex.acquire()
 					for userTuple in activeUsers:
 						if userTuple[0] == reciever:
@@ -235,51 +275,47 @@ def client_connection_thread(clientSocket):
 							break
 					userListMutex.release()
 
+					#notify user that recipent does not exist or is not logged in
 					if not found:
 						clientSocket.send("DNE".encode())
 						message = clientSocket.recv(4096).decode()
 						if(message != "received"):
 							print("Error receving DNE?")
+					#send message to recipent
 					else:
 						gettingUser = False
 						clientSocket.send("message".encode())
 						message = clientSocket.recv(4096).decode()
-						#recieverSocket.send(message.encode())
 						udpSocket.sendto(message.encode(), recieverAddress)
 						udpSocket.sendto(username.encode(), recieverAddress)
 						udpSocket.sendto("Direct Message (DM)".encode(), recieverAddress)
 						clientSocket.send("complete".encode())
-				#send client list of online users
-				#wait for username AND message
-				#check that user exists and is online
-				#forward message to reciever's socket
-				#senc client message sent confirmation or User Does Not Exist / User is NOt Online message
 				#return to wait for command
 
 		elif operation == "EX":
 			running = False
+			#update list of logged in threads
 			userListMutex.acquire()
 			for userTuple in activeUsers:
 				if userTuple[0] == username:
 					activeUsers.remove(userTuple)
 					break
 			userListMutex.release()
+			#notify client that it was logged out, then close socket
 			clientSocket.send("logout".encode())
 			clientSocket.close()
-			#EX
-				#close socket
-				#update list of logged in threads
-				#client closes it's own socket
-				#allow this thread to end by exiting while loop and reaching end of the function
+			#allow this thread to end by exiting while loop and reaching end of the function
 
 		else:
 			clientSocket.send("unknown".encode())
 			#unknown
 
 
+#main thread for handling receiving new connections
 print("Server ready to recieve connections")
 while True:
-	while(len(connectionList) > maxConnections):
+	#blocks if there are more than the max collections allowed
+	while(len(connectionList) >= maxConnections):
 			tempList.clear()
 			for thread in connectionList:
 				if thread.is_alive():
